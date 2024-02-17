41ad8984b388760f631ed9916cf3d64c95dacff3
kmoon2437 <kmoon2437@gmail.com>
2024-02-17 10:52:47 +0900

    실수로 집어넣은 test 파일 삭제

--- src/test.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import { MidiFile } from './index.js';
-
-let mfile = new MidiFile(fs.readFileSync(new URL('../test/girlsLegendU.mid', import.meta.url)));
-
-console.log(mfile)
