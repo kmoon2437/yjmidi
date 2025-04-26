9e6df6f033f84d5716bd6334ce5ad64c577993c0
kmoon2437 <kmoon2437@gmail.com>
2025-04-27 05:13:03 +0900

    TextMetaEvent의 내용을 Uint8Array로 저장하도록 수정

--- src/event/meta/TextMetaEvent.ts
+++ src/event/meta/TextMetaEvent.ts
@@ -4,36 +4,42 @@ import { ByteStream } from 'byte-data-stream';
 
 export abstract class BaseTextMetaEvent extends MetaEvent {
     abstract readonly subtype: MetaEventType;
-    content: string;
+    content: Uint8Array;
 
     /**
      * 텍스트 문자열을 직접 받는 생성자
      * @param content 텍스트 내용
      */
-    constructor(content: string);
+    constructor(contentText: string);
 
     /**
      * Uint8Array 데이터를 받아 변환하는 생성자
      * @param data 바이너리로 인코딩된 텍스트 데이터
      */
-    constructor(data: Uint8Array);
+    constructor(content: Uint8Array);
 
-    constructor(data: string | Uint8Array) {
+    constructor(content: string | Uint8Array) {
         super();
-        if (typeof data == 'string') this.content = data;
-        else if (data instanceof Uint8Array) this.content = new TextDecoder().decode(data);
-        else this.content = '';
+        if (content instanceof Uint8Array) this.content = content;
+        else if (typeof content == 'string') this.content = new TextEncoder().encode(content);
+        else this.content = new Uint8Array();
     }
 
     serialize(): Uint8Array {
-        let encodedContent = new TextEncoder().encode(this.content);
-
         // meta event 표시 1바이트 + subtype 1바이트 + 길이 최소 1바이트 = 최소 3바이트
-        let bs = new ByteStream(3 + encodedContent.length);
+        let bs = new ByteStream(3 + this.content.length);
         bs.writeBytes([EventType.META, this.subtype]);
-        bs.writeVarUint(encodedContent.length);
-        bs.writeBytes(encodedContent);
+        bs.writeVarUint(this.content.length);
+        bs.writeBytes(this.content);
         return new Uint8Array(bs.buffer);
+    }
+    
+    get contentText() {
+        return new TextDecoder().decode(this.content);
+    }
+
+    set contentText(text) {
+        this.content = new TextEncoder().encode(text);
     }
 }
 
